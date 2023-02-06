472e857ca47c2c68f1399c8258d76a903701dde8
Elissa Yanez <Elissa.Yanez.25@cmc.edu>
2023-02-05 18:52:29 -0800

    first change

--- HTML_Validator.py
+++ HTML_Validator.py
@@ -3,7 +3,8 @@
 
 def validate_html(html):
     '''
-    This function performs a limited version of html validation by checking whether every opening tag has a corresponding closing tag.
+    This function performs a limited version of html validation
+    by checking whether every opening tag has a corresponding closing tag.
 
     >>> validate_html('<strong>example</strong>')
     True
